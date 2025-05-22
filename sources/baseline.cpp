524d7f297a114cbe6ee0833e0d481b65bda398f8
moritz techen <moritz.techen@gmail.com>
2025-05-22 16:35:12 +0200

    finished baseline

--- sources/baseline.cpp
+++ sources/baseline.cpp
@@ -7,15 +7,13 @@
 struct HeapNode {
     int idx;      // index of the array
     int pos;      // position in the array
-    char* ptr;    // pointer to the element
+    tuwtype_t* ptr;    // pointer to the element
 };
 
 struct HeapCompare {
-    MPI_Datatype type;
-    int typesize;
     bool operator()(const HeapNode& a, const HeapNode& b) const {
         // Compare the elements pointed by a.ptr and b.ptr
-        return std::memcmp(a.ptr, b.ptr, typesize) > 0;
+        return *(a.ptr) > *(b.ptr); // Min-heap
     }
 };
 
@@ -26,8 +24,6 @@ int HPC_AllgatherMergeBase(const void *sendbuf, int sendcount,
     MPI_Comm_rank(comm, &rank);
     MPI_Comm_size(comm, &size);
 
-    int typesize;
-    MPI_Type_size(sendtype, &typesize);
 
     // 1. Allgather all data
     MPI_Allgather(
@@ -37,7 +33,6 @@ int HPC_AllgatherMergeBase(const void *sendbuf, int sendcount,
     );
 
     // 2. Prepare for p-way merge
-    // Each process's data is at ((char*)recvbuf) + i * recvcount * typesize
     std::vector<HeapNode> heap;
     heap.reserve(size);
 
@@ -45,18 +40,18 @@ int HPC_AllgatherMergeBase(const void *sendbuf, int sendcount,
         if (recvcount > 0) {
             heap.push_back(HeapNode{
                 i, 0,
-                ((char*)recvbuf) + i * recvcount * typesize
+                static_cast<tuwtype_t*>(recvbuf) + i * recvcount
             });
         }
     }
 
     std::vector<int> positions(size, 0);
     std::priority_queue<HeapNode, std::vector<HeapNode>, HeapCompare> pq(
-        HeapCompare{recvtype, typesize}, heap
+        HeapCompare{}, heap
     );
 
     // Allocate temporary output buffer
-    std::vector<char> output(size * recvcount * typesize);
+    std::vector<tuwtype_t> output(size * recvcount);
 
     for (int out_idx = 0; out_idx < size * recvcount; ++out_idx) {
         if (pq.empty()) break;
@@ -64,9 +59,9 @@ int HPC_AllgatherMergeBase(const void *sendbuf, int sendcount,
 
         // Copy the element to output
         std::memcpy(
-            output.data() + out_idx * typesize,
+            output.data() + out_idx,
             min.ptr,
-            typesize
+            sizeof(tuwtype_t)
         );
 
         // Advance in the corresponding array
@@ -75,13 +70,13 @@ int HPC_AllgatherMergeBase(const void *sendbuf, int sendcount,
             pq.push(HeapNode{
                 min.idx,
                 positions[min.idx],
-                ((char*)recvbuf) + min.idx * recvcount * typesize + positions[min.idx] * typesize
+                static_cast<tuwtype_t*>(recvbuf) + min.idx * recvcount + positions[min.idx]
             });
         }
     }
 
     // Copy merged output back to recvbuf
-    std::memcpy(recvbuf, output.data(), size * recvcount * typesize);
+    std::memcpy(recvbuf, output.data(), size * recvcount * sizeof(tuwtype_t));
 
     return MPI_SUCCESS;
 }
