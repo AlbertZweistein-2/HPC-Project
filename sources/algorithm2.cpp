2978af8d9b184954455fde69f9e55cc44cfc765c
sudo-whoami <moritz.techen@gmail.com>
2025-06-01 16:12:21 +0200

    new vers

--- sources/algorithm2.cpp
+++ sources/algorithm2.cpp
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <cmath>
 #include <vector>
+#include <algorithm>
 #include "algorithms.h"
 
 #include <iostream>
@@ -18,9 +19,11 @@ void twowaymerge(const void *X, const void *Y, void *Z, int n) {
       z[k++] = y[j++];
     }
   }
+
   while (i < n) {
     z[k++] = x[i++];
   }
+
   while (j < n) {
     z[k++] = y[j++];
   }
@@ -52,14 +55,24 @@ int HPC_AllgatherMergeCirculant(const void *sendbuf, int sendcount, MPI_Datatype
   */
 
   // Temporary buffers for merging
-  std::vector<tuwtype_t> W(recvcount, 0);
-  std::vector<tuwtype_t> T(recvcount, 0);
-  std::vector<tuwtype_t> W_prime(recvcount, 0);
+  std::vector<tuwtype_t> T;
+  T.reserve(size * sendcount);
+  std::vector<tuwtype_t> W;
+  W.reserve(size * sendcount);
+  std::vector<tuwtype_t> W_prime;
+  W_prime.reserve(size * sendcount);
 
   for (int k = 0; k < q; k++) {
     int epsilon = s_k[k + 1] & 0x1;
     int t = (rank - s_k[k] + epsilon + size) % size;
     int f = (rank + s_k[k] - epsilon) % size;
+
+    // Debug print for current step
+    /*
+    std::cout << "Rank " << rank << ", k = " << k 
+              << ", t = " << t << ", f = " << f 
+              << ", epsilon = " << epsilon << std::endl;
+    */
 
     if (epsilon == 1) {
       // Send W to process t and receive T from process f
@@ -67,34 +80,66 @@ int HPC_AllgatherMergeCirculant(const void *sendbuf, int sendcount, MPI_Datatype
                    T.data(), sendcount, sendtype, f, 0, 
                    comm, MPI_STATUS_IGNORE);
 
-      // Merge W and T into W
-      twowaymerge(W.data(), T.data(), W.data(), recvcount);
+      // Merge W and T into M
+      std::merge(W.begin(), W.end(), T.begin(), T.end(), W.begin());
+
     } else {
       if (k == 0) {
         // Send V (initial data) to process t and receive W from process f
-        MPI_Sendrecv(sendbuf, sendcount, sendtype, t, 0,
+        MPI_Sendrecv(static_cast<const tuwtype_t*>(sendbuf), sendcount, sendtype, t, 0,
                      W.data(), sendcount, sendtype, f, 0, 
                      comm, MPI_STATUS_IGNORE);
+
+        // Debug
+        if (rank == 0) {
+          std::cout << "Rank " << rank << ", initial W size: " 
+                    << W.size() << ", contents: ";
+          for (const auto& item : W) {
+            std::cout << item << " ";
+          }
+          std::cout << std::endl;
+        }
       } else {
         // Merge V and W into W_prime
-        twowaymerge(sendbuf, W.data(), W_prime.data(), recvcount);
+        std::merge(
+          static_cast<const tuwtype_t*>(sendbuf),
+          static_cast<const tuwtype_t*>(sendbuf) + sendcount,
+          W.begin(),
+          W.end(),
+          W_prime.begin()
+        );
 
         // Send W_prime to process t and receive T from process f
-        MPI_Sendrecv(W_prime.data(), recvcount, sendtype, t, 0,
-                     T.data(), recvcount, sendtype, f, 0, 
+        MPI_Sendrecv(W_prime.data(), W_prime.size(), sendtype, t, 0,
+                     T.data(), W_prime.size(), sendtype, f, 0, 
                      comm, MPI_STATUS_IGNORE);
 
         // Merge W and T into W
-        twowaymerge(W.data(), T.data(), W.data(), recvcount);
+        std::merge(W.begin(), W.end(), T.begin(), T.end(), W.data());
       }
+    }
+
+    // Debug print for merged W
+    if (rank == 0) {
+      std::cout << "Rank " << rank << ", after merge W size: " 
+                << W.size() << ", contents: ";
+      for (const auto& item : W) {
+        std::cout << item << " ";
+      }
+      std::cout << std::endl;
     }
   }
 
-  // Final merge of V and W into W
-  twowaymerge(sendbuf, W.data(), W_prime.data(), recvcount);
+  std::merge(
+    static_cast<const tuwtype_t*>(sendbuf),
+    static_cast<const tuwtype_t*>(sendbuf) + sendcount,
+    W.begin(),
+    W.end(),
+    W.begin()
+  );
 
   // Copy the final result into recvbuf
-  memcpy(recvbuf, W_prime.data(), recvcount * sizeof(tuwtype_t));
+  memcpy(recvbuf, W.data(), size * sendcount * sizeof(tuwtype_t));
 
   return MPI_SUCCESS;
 }
